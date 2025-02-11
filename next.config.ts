b11b1c7b584c4aa27854b73050c66a6a8143ed04
Belat <enochbelawu8@gmail.com>
2025-02-11 14:19:22 +0100

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
   };
   
   module.exports = {
@@ -20,6 +15,9 @@ const nextConfig: NextConfig = {
           search: '',
         },
       ],
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
     },
 };
 
